727bb39a2a871a327ab72ebf63b9455002b5db83
Serhii Tkachenko <doker.tk@gmail.com>
2022-08-28 02:13:28 +0200

    created tableView with default cell

--- Setting Screen/Sources/View/SettingViewController.swift
+++ Setting Screen/Sources/View/SettingViewController.swift
@@ -12,6 +12,13 @@ class SettingViewController: UIViewController {
 
     // MARK: - Outlets
 
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.rowHeight = 50
+        return tableView
+    }()
 
     // MARK: - Lifecycle
 
@@ -27,11 +34,14 @@ class SettingViewController: UIViewController {
     // MARK: - Setup
 
     private func setupHierarchy() {
+        view.addSubview(tableView)
 
     }
 
     private func setupLayout() {
-
+        tableView.snp.makeConstraints { make in
+            make.top.bottom.left.right.equalTo(view)
+        }
     }
 
     // MARK: - Actions
@@ -39,3 +49,18 @@ class SettingViewController: UIViewController {
 
 }
 
+extension SettingViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        guard let cell = cell else { return UITableViewCell() }
+        cell.imageView?.image = UIImage(systemName: "star")
+        cell.textLabel?.text = "Test"
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+
+}
