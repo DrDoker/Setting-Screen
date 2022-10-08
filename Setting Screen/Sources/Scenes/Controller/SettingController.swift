e3e92536e0bb76684bc59460b611c34713a6e979
Serhii Tkachenko <doker.tk@gmail.com>
2022-10-08 13:36:42 +0200

    Preliminary separation SettingView

--- Setting Screen/Sources/Scenes/Controller/SettingController.swift
+++ Setting Screen/Sources/Scenes/Controller/SettingController.swift
@@ -8,69 +8,52 @@
 import UIKit
 
 final class SettingController: UIViewController {
+    private var model: [[Setting]]?
 
-    private var settingCells: [[Setting]]?
-
-    // MARK: - Outlets
-
-    private lazy var search: UISearchController = {
-        let search = UISearchController()
-        search.searchBar.placeholder = "Поиск"
-        return search
-    }()
-
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .insetGrouped)
-        tableView.register(StandartTableViewCell.self, forCellReuseIdentifier: StandartTableViewCell.identifier)
-        tableView.register(UserInfoTableViewCell.self, forCellReuseIdentifier: UserInfoTableViewCell.identifier)
-        tableView.register(FamilyInfoTableViewCell.self, forCellReuseIdentifier: FamilyInfoTableViewCell.identifier)
-        tableView.dataSource = self
-        tableView.delegate = self
-        return tableView
-    }()
+    private var settingView: SettingView? {
+        guard isViewLoaded else { return nil }
+        return view as? SettingView
+    }
 
     // MARK: - Lifecycle
 
-    override func viewWillAppear(_ animated: Bool) {
-        tableView.reloadData()
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view = SettingView()
+        model = Setting.settingCells
+        
         title = "Настройки"
         navigationController?.navigationBar.prefersLargeTitles = true
-        view.backgroundColor = .systemGray6
-        setupHierarchy()
-        setupLayout()
-
-        settingCells = Setting.settingCells
-    }
-
-    // MARK: - Setup
-
-    private func setupHierarchy() {
-        navigationItem.searchController = search
-        view.addSubview(tableView)
-    }
-
-    private func setupLayout() {
-        tableView.snp.makeConstraints { make in
-            make.top.left.right.bottom.equalTo(view)
-        }
+        configTable()
+        setupNavigationBar()
     }
 }
 
-extension SettingController: UITableViewDataSource, UITableViewDelegate {
+extension SettingController {
+    func setupNavigationBar() {
+        navigationItem.searchController = settingView?.search
+    }
+}
+
+extension SettingController {
+    func configTable() {
+        settingView?.tableView.dataSource = self
+        settingView?.tableView.delegate = self
+    }
+}
+
+extension SettingController: UITableViewDelegate, UITableViewDataSource  {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return settingCells?.count ?? 0
+        return model?.count ?? 0
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return settingCells?[section].count ?? 0
+        return model?[section].count ?? 0
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        guard let cellType = settingCells?[indexPath.section][indexPath.row].type else { return 44 }
+        guard let cellType = model?[indexPath.section][indexPath.row].type else { return 44 }
 
         switch cellType {
         case .userInfo:
@@ -83,65 +66,50 @@ extension SettingController: UITableViewDataSource, UITableViewDelegate {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = settingCells?[indexPath.section][indexPath.row] else { return UITableViewCell() }
-        return configureCell(cell: cell, indexPath: indexPath)
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        guard let cell = settingCells?[indexPath.section][indexPath.row] else { return }
-        print("Вы нажали \(cell.title)")
-
-        switch cell.type {
-        case .cellWithSwitch:
-            return
-        case .userInfo, .familyInfo, .standart:
-            let settingCell = settingCells?[indexPath.section][indexPath.row]
-            let detailViewController = DetailController(settingCell: settingCell)
-            navigationController?.pushViewController(detailViewController, animated: true)
-        }
-    }
-
-    private func configureCell(cell: Setting, indexPath: IndexPath) -> UITableViewCell {
-        let settingCells = settingCells?[indexPath.section][indexPath.row]
+        guard let cell = model?[indexPath.section][indexPath.row] else { return UITableViewCell() }
 
         switch cell.type {
         case .userInfo:
             let userInfoCell = tableView.dequeueReusableCell(withIdentifier: UserInfoTableViewCell.identifier) as? UserInfoTableViewCell
             guard let userInfoCell = userInfoCell else { return UITableViewCell() }
-            userInfoCell.userInfoCell = settingCells
+            userInfoCell.userInfoCell = cell
             userInfoCell.accessoryType = .disclosureIndicator
             return userInfoCell
         case .familyInfo:
             let familyInfoCell = tableView.dequeueReusableCell(withIdentifier: FamilyInfoTableViewCell.identifier) as? FamilyInfoTableViewCell
             guard let familyInfoCell = familyInfoCell else { return UITableViewCell() }
-            familyInfoCell.settingCell = settingCells
+            familyInfoCell.settingCell = cell
             familyInfoCell.setIcons(first: UIImage(named: "anna1"), second: UIImage(named: "anna2"))
             familyInfoCell.accessoryType = .disclosureIndicator
             return familyInfoCell
         case .standart:
             let standartCell = tableView.dequeueReusableCell(withIdentifier: StandartTableViewCell.identifier) as? StandartTableViewCell
             guard let standartCell = standartCell else { return UITableViewCell() }
-            standartCell.settingCell = settingCells
+            standartCell.settingCell = cell
             standartCell.accessoryType = .disclosureIndicator
             return standartCell
         case .cellWithSwitch:
             let standartCell = tableView.dequeueReusableCell(withIdentifier: StandartTableViewCell.identifier) as? StandartTableViewCell
             guard let standartCell = standartCell else { return UITableViewCell() }
-            standartCell.settingCell = settingCells
+            standartCell.settingCell = cell
             standartCell.selectionStyle = .none
             return standartCell
         }
     }
-}
 
-extension UITextField {
-    func setLeftIcon(_ image: UIImage?) {
-        let iconView = UIImageView(frame: CGRect(x: 5, y: 0, width: 20, height: 20))
-        iconView.image = image
-        let iconContainerView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 20))
-        iconContainerView.addSubview(iconView)
-        leftView = iconContainerView
-        leftViewMode = .always
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let cell = model?[indexPath.section][indexPath.row] else { return }
+        print("Вы нажали \(cell.title)")
+
+        switch cell.type {
+        case .cellWithSwitch:
+            return
+        case .userInfo, .familyInfo, .standart:
+            let settingCell = model?[indexPath.section][indexPath.row]
+            let detailController = DetailController(settingCell: settingCell)
+            navigationController?.pushViewController(detailController, animated: true)
+        }
     }
 }
+
--- Setting Screen/Sources/Scenes/View/SettingView.swift
+++ Setting Screen/Sources/Scenes/View/SettingView.swift
@@ -10,4 +10,49 @@ import SnapKit
 
 class SettingView: UIView {
 
+    // MARK: - Outlets
+
+    lazy var search: UISearchController = {
+        let search = UISearchController()
+        search.searchBar.placeholder = "Поиск"
+        return search
+    }()
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.register(StandartTableViewCell.self, forCellReuseIdentifier: StandartTableViewCell.identifier)
+        tableView.register(UserInfoTableViewCell.self, forCellReuseIdentifier: UserInfoTableViewCell.identifier)
+        tableView.register(FamilyInfoTableViewCell.self, forCellReuseIdentifier: FamilyInfoTableViewCell.identifier)
+        return tableView
+    }()
+
+    //MARK: - Initializers
+
+    init() {
+        super.init(frame: .zero)
+        commonInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        backgroundColor = .systemGray6
+        setupHierarchy()
+        setupLayout()
+    }
+
+    // MARK: - Setup
+
+    private func setupHierarchy() {
+        addSubview(tableView)
+    }
+
+    private func setupLayout() {
+        tableView.snp.makeConstraints { make in
+            make.top.left.right.bottom.equalTo(self)
+        }
+    }
 }
